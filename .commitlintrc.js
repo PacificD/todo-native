839f65bfa22e6a5f3087d8b154b8dd8c29d3a5be
PacificD <1484636442@qq.com>
2022-07-13 17:22:12 +0800

    feat(test): test
    test

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+/**
+ * @Author: Pacific_D
+ * @Date: 2022-07-13 17:14:15
+ * @LastEditTime: 2022-07-13 17:14:15
+ * @LastEditors: Pacific_D
+ * @Description: 
+ * @FilePath: \react-demo\.commitlintrc.js
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional']
+};
