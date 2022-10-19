9e426ad3a187b6383f951acce92c1de046fc11e3
PacificD <1484636442@qq.com>
2022-10-19 22:58:52 +0800

    feat(componenets): themeToggle icon

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,24 +1,39 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-08-04 20:48:01
- * @LastEditTime: 2022-08-04 20:49:57
+ * @LastEditTime: 2022-10-19 22:57:25
  * @LastEditors: Pacific_D
  * @Description:
- * @FilePath: \todo-native\src\components\theme-toggle.tsx
+ * @FilePath: \todo-native\src\components\ThemeToggle.tsx
  */
-import React, { FC } from "react"
-import { Text, HStack, Switch, useColorMode } from "native-base"
+import { FC } from "react"
+import {
+  Text,
+  HStack,
+  Switch,
+  useColorMode,
+  useColorModeValue
+} from "native-base"
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons"
 
 const ThemeToggle: FC = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
     <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
+      <Ionicons
+        name="moon"
+        size={24}
+        color={useColorModeValue("#282c3f", "#eee")}
+      />
       <Switch
         isChecked={colorMode === "light"}
         onToggle={toggleColorMode}
       ></Switch>
-      <Text>Light</Text>
+      <MaterialCommunityIcons
+        name="lightbulb-on"
+        size={24}
+        color={useColorModeValue("#282c3f", "#eee")}
+      />
     </HStack>
   )
 }
