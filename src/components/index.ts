cf4663ed6311e88f7a3e26a232ad27d10091544e
PacificD <1484636442@qq.com>
2022-10-19 21:47:42 +0800

    refactor(components): renamed

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Pacific_D
+ * @Date: 2022-10-19 21:11:46
+ * @LastEditTime: 2022-10-19 21:11:47
+ * @LastEditors: Pacific_D
+ * @Description:
+ * @FilePath: \todo-native\src\components\index.ts
+ */
+import AppContainer from "./AppContainer"
+import InputField from "./InputField"
+import ThemeToggle from "./ThemeToggle"
+
+export default {
+  AppContainer,
+  InputField,
+  ThemeToggle
+}
